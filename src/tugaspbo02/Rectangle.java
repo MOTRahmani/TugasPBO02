dc490b1d84ac7f469e4ca382182e817de5d6e42b
Muhammad Rahmani <muhammadrahmani84@gmail.com>
2018-04-02 18:02:58 +0800

    Membuat attribut dan behavior

--- src/tugaspbo02/Rectangle.java
+++ src/tugaspbo02/Rectangle.java
@@ -1,5 +1,13 @@
 package tugaspbo02;
 
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("=*=*=*=*=*=*=*=*=*=");
+        System.out.println("panjang :"+panjang);
+        System.out.println("lebar   :"+lebar);
+        System.out.println("=*=*=*=*=*=*=*=*=*=");
+    }
 }
